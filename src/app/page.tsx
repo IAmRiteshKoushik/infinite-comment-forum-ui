1d0c7e2bcb25fc004e72f3c9c13727f0e9f1018f
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-02-22 00:36:42 +0530

    (done) Nested comment component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,58 @@
 "use client";
+
 import { useState } from 'react';
+
+interface CommentItemProps {
+    comment: Comment;
+}
 
 interface Comment {
     body: string;
-    comment: Array<Comment>; // This is where the recursion happens
+    comments: Array<Comment>; // This is where the recursion happens
 };
+
+interface commentInputProps {
+    // This takes in a newComment of the type Comment
+    // And it returns nothing
+    onComment: (newComment: Comment) => void;
+}
 
 const dummyComments: Array<Comment> = [
     {
         body: "This is a comment",
-        comment: [],
+        comments: [],
     },
     {
         body: "This is second comment",
-        comment: [],
+        comments: [],
     },
     {
         body: "This is third comment",
-        comment: [],
+        comments: [],
     }
 ];
 
 export default function Home() {
 
     const[comments, setComments] = useState(dummyComments);
-    const [commentBody, setCommentBody] = useState(''); // empty string
-
-    const onComment = () => {
-        const newComment: Comment = {
-            body: commentBody, 
-            comment: [],
-        };
+    const onComment = (newComment: Comment) => {
         // Alternate method:
         // setComments(prev => [newComment, ...prev]);
         // Convenient method : 
-        setComments([newComment, ...comments]);
-        setCommentBody(""); // Resetting after comment is created
+        setComments(prev => [...comments, newComment]);
     }
-
-    // In Nested Comments system :
-    // 1 - Every comment has its own dynamically generated state variables
-    // 2 - This 
 
     return(
     <div className="flex flex-col gap-6 h-screen w-screen p-6">
-        <span className="">React Nested Components</span>
+        <span className="">Nested Comment Component</span>
+        {/* Zeroth Comment : Starts the thread */}
         <div>
-            <div className="flex flex-col">
+            {/*<div className="flex flex-col">
                 {/* 
                 We want the state to update as and when the comment is written
                 and we need it to update on every keystroke.
                 */}
-                <input 
+                {/*<input 
                     value={commentBody}
                     onChange={event => setCommentBody(event.target.value)}
                     placeholder="What are your thoughts ?" 
@@ -63,7 +64,8 @@ export default function Home() {
                 >
                     Comment
                 </button>
-            </div>
+            </div>*/}
+            <CommentInput onComment={onComment}/>
             <div className="flex flex-col gap-4 mt-10">
                 {comments.map((comment: Comment) => (
                     <CommentItem comment={comment} />
@@ -74,9 +76,6 @@ export default function Home() {
     );
 }
 
-interface CommentItemProps {
-    comment: Comment;
-}
 
 // Inserting comments
 const CommentItem = ({ comment }: CommentItemProps) => {
@@ -85,10 +84,14 @@ const CommentItem = ({ comment }: CommentItemProps) => {
     const [isReplying, setIsReplying] = useState(false);
     // In nested comments, each comment has it's own set 
     // of comments so that has to be a separate state 
+    const [comments, setComments] = useState(comment.comments);
 
+    const onComment = (newComment: Comment) => {
+        setComments(prev => [...prev, newComment]);
+    };
 
     return(
-        <div className="flex flex-col border-[1px] border-zinc-500 rounded-md">
+        <div className="flex flex-col border-[1px] border-zinc-500 rounded-md p-2">
             <span>{comment.body}</span>
             {isReplying ? 
             <button
@@ -103,13 +106,46 @@ const CommentItem = ({ comment }: CommentItemProps) => {
             >
                Reply 
             </button>}
-            {isReplying && (
+            {/*isReplying && (
                 <input 
                 placeholder="What are your thoughts?"
                 className="border-[1px] border-zinc-400 p-4 w-3/4"
                 />
-            )}
+            )*/}
+            {isReplying && <CommentInput onComment={onComment}/>}
+            <div className="flex flex-col gap-4 mt-10">
+                {comments.map((comment: Comment) => (
+                    <CommentItem comment={comment} />
+                ))}
+            </div>
         </div>
-    )
-
+    );
 }
+
+// Making a re-usable comment-input box
+const CommentInput = ({ onComment }: commentInputProps) => {
+
+    const [commentBody, setCommentBody] = useState(''); // empty string
+
+    return (
+        <div className='flex flex-col'>
+        <input 
+            value={commentBody}
+            onChange={event => setCommentBody(event.target.value)}
+            placeholder="What are your thoughts ?" 
+            className="border-[1px] border-zinc-400 p-4 w-3/4"
+        />
+        <button
+            className="border-[1px] rounded-full border-zinc-400 w-20"
+            onClick={() => {
+                onComment({ body: commentBody, comments: [] });
+                setCommentBody(""); // Re-setting the body to be empty string
+                }
+            }
+        >
+                Comment
+        </button>
+        </div>
+    );
+}
+
